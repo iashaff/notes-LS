fc035cc9870e3bf52d2317dfaf92a564ccf72635
iashaff <optimistka30@mail.ru>
2023-07-23 20:04:01 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Notes react app from The Frontend Developer Career Path scrimba.com
+You can add note. update note and delete note.
+Using:
+1. Vite
+2. Local Storage
+3. UseState, useEffect hooks
+
+Dependencies:
+1. nanoid
+2. react-mde
+3. react-split
+4. showdown
+
---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { data } from './data.js'
+import Editor from './components/Editor'
+import Sidebar from './components/Sidebar'
+import Split from 'react-split'
+import { nanoid } from 'nanoid'
+
+export default function App() {
+  const [notes, setNotes] = useState( () => JSON.parse(localStorage.getItem('notes')) || [] )
+
+    const [currentNoteId, setCurrentNoteId] = useState(
+        (notes[0] && notes[0].id) || ""
+    )
+
+    useEffect(() => {
+       localStorage.setItem('notes', JSON.stringify(notes))
+    }, [notes])
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+    
+    function updateNote(text) {
+      setNotes(oldNotes => {
+          const newArray = []
+          for (let i = 0; i < oldNotes.length; i++) {
+              const oldNote = oldNotes[i]
+              if (oldNote.id === currentNoteId) {
+                  // Put the most recently-modified note at the top
+                  newArray.unshift({ ...oldNote, body: text })
+              } else {
+                  newArray.push(oldNote)
+              }
+          }
+          return newArray
+      })
+  }
+
+    // Deleting notes by click
+
+    function deleteNote(event, noteId) {
+      event.stopPropagation()
+      setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId))
+    }
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
+}
